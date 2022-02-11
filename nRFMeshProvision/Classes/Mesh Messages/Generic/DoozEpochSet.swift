b8a61172de02f33581b786cc939a25c6236cc965
R0m4in-dooz <romain@dooz-domotique.com>
2022-02-11 16:20:03 +0100

    fix parsing

--- nRFMeshProvision/Classes/Mesh Messages/Generic/DoozEpochSet.swift
+++ nRFMeshProvision/Classes/Mesh Messages/Generic/DoozEpochSet.swift
@@ -32,20 +32,21 @@
 import Foundation
 
 public struct DoozEpochSet: AcknowledgedGenericMessage, TransactionMessage {
-    public var tid: UInt8!
+    public var tId: UInt8!
     public static let opCode: UInt32 = 0x8220
     public static let responseType: StaticMeshMessage.Type = DoozEpochStatus.self
 
     public var parameters: Data? {
         var data = Data() + tId
-        let uTz = (mTzData & 0x1FF)
-        print("📣mTzData: \(mTzData) (\(String(mTzData, 2)))")
-        print("📣mTzData & 0x1FF: \(uTz) (\(String(uTz, 2)))")
-        print("📣mCommand: \(mCommand) (\(String(mCommand, 2)))")
-        print("📣mIO: \(mIO) (\(String(mIO, 2)))")
-        print("📣mUnused: \(mUnused) (\(String(mUnused, 2)))")
-        data += UInt16(mUnused << 14 | mIO << 13 | mCommand << 9 | ((uTz << 8) & 0x7) | uTz)
-        print("📣packed: \(packed) (\(String(packed, 2)))")
+        let uTz = UInt16(mTzData & 0x1FF)
+        print("📣mTzData: \(mTzData) (\(String(mTzData, radix: 2)))")
+        print("📣mTzData & 0x1FF: \(uTz) (\(String(uTz, radix: 2)))")
+        print("📣mCommand: \(mCommand) (\(String(mCommand, radix: 2)))")
+        print("📣mIO: \(mIO) (\(String(mIO, radix: 2)))")
+        print("📣mUnused: \(mUnused) (\(String(mUnused, radix: 2)))")
+        let packed = UInt8(mUnused << 6 | mIO << 5 | mCommand << 1 | ((uTz << 8) & 0x7)) | UInt8(uTz)
+        print("📣packed: \(packed) (\(String(packed, radix: 2)))")
+        data += packed
         print("📣mEpoch: \(mEpoch)")
         print("📣mCorrelation: \(mCorrelation)")
         data += mEpoch
@@ -79,34 +80,35 @@ public struct DoozEpochSet: AcknowledgedGenericMessage, TransactionMessage {
         self.mTzData = tzData
         self.mUnused = command
         self.mIO = io
-        self.mUnused= unused
+        self.mUnused = unused
         self.mEpoch = epoch
         self.mCorrelation = correlation
         self.mExtra = extra
     }
 
     public init?(parameters: Data) {
-        tid = parameters[0]
-        let packed = parameters.read(fromOffset: 1)
-        print("📣packed: \(packed) (\(String(packed, 2)))");
+        tId = parameters[0]
+        let packed = UInt16(parameters.read(fromOffset: 1))
+        print("📣packed: \(packed) (\(String(packed, radix: 2)))");
         self.mUnused = packed >> 14;
-        print("📣mUnused: \(mUnused) (\(String(mUnused, 2)))");
+        print("📣mUnused: \(mUnused) (\(String(mUnused, radix: 2)))");
         self.mIO = (packed >> 13) & 0x1;
-        print("📣mIO: \(mIO) (\(String(mIO, 2)))");
+        print("📣mIO: \(mIO) (\(String(mIO, radix: 2)))");
         self.mCommand = (packed >> 9) & 0xF;
-        print("📣mCommand: \(mCommand) (\(String(mCommand, 2)))");
-        var int uTz = packed & 0x1FF;
+        print("📣mCommand: \(mCommand) (\(String(mCommand, radix: 2)))");
+        let uTz = packed & 0x1FF;
         // MeshParserUtils.unsignedToSigned from Android-nRF-Mesh-Library
         if ((uTz & (1 << 9 - 1)) != 0) {
             uTz = -1 * ((1 << 9 - 1) - (uTz & ((1 << 9 - 1) - 1)));
         }
-        print("📣mTzData: \(mTzData) (\(String(uTz, 2)))");
-        self.mEpoch = parameters.read(fromOffset: );
+        self.mTzData = uTz
+        print("📣mTzData: \(mTzData) (\(String(uTz, radix: 2)))");
+        self.mEpoch = parameters.read(fromOffset: 3);
         print("📣mEpoch: \(mEpoch)");
-        self.mCorrelation = parameters.read(fromOffset: );
+        self.mCorrelation = parameters.read(fromOffset: 7);
         print("📣mCorrelation: \(mCorrelation)");
         if parameters.count == 5 {
-            self.mExtra = parameters.read(fromOffset: 1)
+            self.mExtra = parameters.read(fromOffset: 11)
             print("📣mExtra: \(mExtra)");
         } else {
             self.mExtra = nil
